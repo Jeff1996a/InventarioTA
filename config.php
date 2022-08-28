ccb6aadbf8a61d396b91c16993d26e3a6fe43e54
Jefferson Alfaro <69323781+Jeff1996a@users.noreply.github.com>
2022-08-27 21:29:40 -0500

    Update config.php

--- config.php
+++ config.php
@@ -3,13 +3,12 @@
 	/**
  	* Constantes para la base de datos
  	*/
-	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-	define('DB_HOST', 'us-cdbr-east-06.cleardb.net');
-	define('DB_USER', 'b6d703c41b07c3');
-	define('DB_PASSWORD', '5d4c55e4');
-	define('DB_DATABASE_NAME', 'heroku_e4baa52f58ccb53');
-	define('DB_CHARSET', 'UTF8');
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
 
 	/**
  	* Constantes de nombres de archivos
