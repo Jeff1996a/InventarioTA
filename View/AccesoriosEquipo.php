73b66990fa376e91b9cfc5697b665b0c36244920
Jeff996a <jefferson.alfaro@epn.edu.ec>
2022-08-31 18:19:03 -0500

    Equipo tooltips

--- View/AccesoriosEquipo.php
+++ View/AccesoriosEquipo.php
@@ -1,12 +1,12 @@
 <div class="container-fluid">
     <div class="history-header">
-        <div id="btnRegresar" role="button">
+        <div id="btnRegresar" role="button" data-toggle="tooltip" data-placement="bottom" title="Regresar">
             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#fa983a " class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                 <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
             </svg>
         </div>
         <h1><?=$GLOBALS['title']?></h1>
-        <div id="btnAddAccesories" role="button">
+        <div id="btnAddAccesories" role="button" data-toggle="tooltip" data-placement="bottom" title="Nuevo Accesorio">
             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#fa983a " class="bi bi-plus-circle-fill float-end" viewBox="0 0 16 16">
                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
             </svg>
