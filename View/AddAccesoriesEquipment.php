24d08e86f93d8d8c74c507135551ca1ea6fe1e04
Jeff996a <jefferson.alfaro@epn.edu.ec>
2022-09-01 10:49:06 -0500

    update add accesories

--- View/AddAccesoriesEquipment.php
+++ View/AddAccesoriesEquipment.php
@@ -142,7 +142,7 @@
             form_data.append('serie', serie);
             form_data.append('codigoTA', serieTA);
             form_data.append('descripcion', descripcion);
-            form_data.append('disponibiliodad', disp);
+            form_data.append('disponibilidad', disp);
             form_data.append('action', 'addAccesorio');
             form_data.append('id_equipo', id_equipo);
 
