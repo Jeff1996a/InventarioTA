01d85688f7c0232f6cd91a239417427dcab8efd5
Jeff996a <jefferson.alfaro@epn.edu.ec>
2022-09-02 16:22:39 -0500

    Cambio color boton accesorios

--- View/ListaEquipos.php
+++ View/ListaEquipos.php
@@ -107,7 +107,7 @@
                         </a>
                     </td>
                     <td>
-                        <a id="btnAccesorios" role="button" class="text-warning" data-toggle="tooltip" data-placement="bottom" title="Lista de accesorios">
+                        <a id="btnAccesorios" role="button" class="text-primary" data-toggle="tooltip" data-placement="bottom" title="Lista de accesorios">
                             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-pci-card" viewBox="0 0 16 16">
                                 <path d="M0 1.5A.5.5 0 0 1 .5 1h1a.5.5 0 0 1 .5.5V4h13.5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5H2v2.5a.5.5 0 0 1-1 0V2H.5a.5.5 0 0 1-.5-.5Z"/>
                                 <path d="M3 12.5h3.5v1a.5.5 0 0 1-.5.5H3.5a.5.5 0 0 1-.5-.5v-1Zm4 0h4v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1Z"/>
