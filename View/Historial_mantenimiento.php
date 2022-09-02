e8b16c03b713690f4ee9bf7cc35f2418a057d872
Jeff996a <jefferson.alfaro@epn.edu.ec>
2022-09-02 14:29:39 -0500

    Actualización Historial Mantenimiento

--- View/Historial_mantenimiento.php
+++ View/Historial_mantenimiento.php
@@ -1,12 +1,12 @@
 <div class="container-fluid">
     <div class="history-header">
-        <div id="btnRegresar" role="button">
+        <div id="btnRegresar" role="button"  data-toggle="tooltip" data-placement="bottom" title="Regresar">
             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#fa983a " class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                 <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
             </svg>
         </div>
         <h1><?=$GLOBALS['title']?></h1>
-        <div id="btnAddHistory" role="button">
+        <div id="btnAddHistory" role="button" data-toggle="tooltip" data-placement="bottom" title="Agregar registro">
             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#fa983a " class="bi bi-plus-circle-fill float-end" viewBox="0 0 16 16">
                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
             </svg>
@@ -34,6 +34,7 @@
             <td><strong>Problema</strong></td>
             <td><strong>Solución</strong></td>
             <td><strong>Observación</strong></td>
+            <td></td>
 
         </tr>
         </thead>
@@ -57,13 +58,21 @@
                 <td><?php echo $row["problema"]; ?> </td>
                 <td><?php echo $row["solucion"]; ?> </td>
                 <td><?php echo $row["observacion"]; ?> </td>
+                <td>
+                    <a id="btnEliminar" role="button" class="text-danger" data-toggle="tooltip" data-placement="bottom" title="Eliminar historial">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        </svg>
+                    </a>     
+                </td>
             </tr>
             <?php
         }
         ?>
         <thead>
         <tr class="text-center text-white" style="background-color: #005aa9;">
-            <td colspan="16"><strong>Se encontraron <?= $GLOBALS['num_filas'] ?> registros.</strong></td>
+            <td colspan="17"><strong>Se encontraron <?= $GLOBALS['num_filas'] ?> registros.</strong></td>
         </tr>
         </thead>
     </table>
