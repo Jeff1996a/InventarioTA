9dd6672a345adf17485211633d0cafd0619f3f20
Jeff996a <jefferson.alfaro@epn.edu.ec>
2022-09-07 08:57:53 -0500

    accesorios

--- View/AccesoriosEquipo.php
+++ View/AccesoriosEquipo.php
@@ -26,6 +26,7 @@
             <td><strong>Serie</strong></td>
             <td><strong>Serie TA</strong></td>
             <td></td>
+            <td></td>
         </tr>
         </thead>
 
@@ -39,6 +40,13 @@
                 <td><?php echo $row["disponibilidad"]; ?> </td>
                 <td><?php echo $row["serie"]; ?> </td>
                 <td><?php echo $row["serieTa"]; ?> </td>
+                <td>
+                        <a id="btnActualizar" role="button" class="text-warning" data-toggle="tooltip" data-placement="bottom" title="Editar equipo">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+                            </svg>
+                        </a>
+                </td>
                 <td>
                     <a id="btnEliminar" role="button" class="text-danger" data-toggle="tooltip" data-placement="bottom" title="Eliminar accesorio">
                         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
