4d78632baf844348d191eefbbf9944098db9da8a
Jeff996a <jefferson.alfaro@epn.edu.ec>
2022-09-21 16:45:38 -0500

    crud controller incidencias

--- Controller/IncidenciasController.php
+++ Controller/IncidenciasController.php
@@ -83,12 +83,12 @@ elseif($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     elseif(isset($_POST)){
-        
+
         include_once ('../Model/IncidenciasModel.php');
 
         $incidencia = new IncidenciasModel();
 
-        if($_POST['action'] == 'addTransmision'){
+        if($_POST['action'] == 'addIncidencia'){
             $transmision->nombre = $_POST['nombre'];
             $transmision->responsable = $_POST['responsable'];
             $transmision->quien_reporta = $_POST['reporta'];
@@ -145,6 +145,73 @@ elseif($_SERVER["REQUEST_METHOD"] == "POST"){
             else{
                 $row = mysqli_fetch_assoc($incidencia->CrearIncidencia($incidencia));
 
+                $incidencia->result = $row["resultado"];
+            }
+
+            echo json_encode($incidencia);
+
+            die;
+        }
+
+        if($_POST['action'] == 'actualizarIncidenci' ){
+            $transmision->id_incidencia = $_POST['id_incidencia'];
+            $transmision->nombre = $_POST['nombre'];
+            $transmision->responsable = $_POST['responsable'];
+            $transmision->quien_reporta = $_POST['reporta'];
+            $transmision->fecha_sop = $_POST['fecha_sop'];
+            $transmision->fecha_sol = $_POST['fecha_sol'];
+            $transmision->problema = $_POST['prob'];
+            $transmision->solucion = $_POST['sol'];
+            $transmision->observacion = $_POST['obs'];
+
+            if(isset($_FILES['files'])){
+                // Count total files
+                $countfiles = count($_FILES['files']['name']);
+
+                // Upload Location
+                $upload_location = "../Files/";
+
+                // To store uploaded files path
+                $equipment->file_array = array();
+
+                $row = mysqli_fetch_assoc($equipment->CrearEquipo($equipment));
+                $equipment->result = $row["resultado"];
+
+                // Loop all files
+                for($index = 0;$index < $countfiles;$index++){
+
+                    if(isset($_FILES['files']['name'][$index]) && $_FILES['files']['name'][$index] != ''){
+                        // File name
+                        $filename = $_FILES['files']['name'][$index];
+
+                        // Get extension
+                        $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+
+                        // Valid image extension
+                        $valid_ext = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , 'ppt');
+
+                        // Check extension
+                        if(in_array($ext, $valid_ext)){
+
+                            // File path
+                            $path = $upload_location.$filename;
+
+                            // Upload file
+                            if(move_uploaded_file($_FILES['files']['tmp_name'][$index],$path)){
+                                $equipment->file_array[] = $path;
+
+                            }
+                        }
+                    }
+                }
+                echo json_encode($equipment);
+                die;
+            }
+
+            else{
+
+                $row = mysqli_fetch_assoc($incidencia->ActualizarIncidencia($incidencia));
+   
                 $incidencia->result = $row["resultado"];
             }
 
