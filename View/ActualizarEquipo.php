1ae55f073c99a889a428b2c274a84c7f3a0b63c4
Jeff996a <jefferson.alfaro@epn.edu.ec>
2022-09-23 10:36:29 -0500

    vvvv

--- View/ActualizarEquipo.php
+++ View/ActualizarEquipo.php
@@ -91,14 +91,14 @@ $equipment = $GLOBALS['equipment'];
                     <div class="mb-2 col">
                         <label for="txtResponsable" class="col-sm-2 col-form-label">Responsable:</label>
                         <div class="col-sm-6">
-                            <input type="text" class="form-control" id="txtResponsable" name="responsable" value="<?php echo $equipment->responsable; ?>" style="text-transform:uppercase" disabled>
+                            <input type="text" class="form-control" id="txtResponsable" name="responsable" value="'.$equipment->responsable.'" style="text-transform:uppercase" disabled>
                         </div>
                     </div>';
     
                     echo '<div class="mb-2 col">
                         <label for="txtUbicacion" class="col-sm-2 col-form-label">Departamento:</label>
                         <div class="col-sm-6">
-                            <input type="text" class="form-control" id="txtDepartamento" name="ubicacion"  value="<?php echo $equipment->departamento; ?>"  style="text-transform:uppercase">
+                            <input type="text" class="form-control" id="txtDepartamento" name="ubicacion"  value="'.$equipment->departamento.'"  style="text-transform:uppercase">
                         </div>
                     </div>';
 
